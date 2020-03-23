88e466613ebc2ed2931ec2f264fb4ef73b9108f0
cybertekschool <github@cybertekschool.com>
2020-03-23 12:25:22 -0400

    Day09 class

--- src/day09_NestedIf_Ternary/Ternary2.java
+++ src/day09_NestedIf_Ternary/Ternary2.java
@@ -4,7 +4,7 @@ public class Ternary2 {
 
     public static void main(String[] args) {
 
-        //ternary: ? means if, : else
+        //ternary: ? means if, : means else
         String result = "";
         if(9 > 10){
            result= "9 is greater";
@@ -17,6 +17,34 @@ public class Ternary2 {
         System.out.println(result);
         System.out.println(result2);
 
+        System.out.println("==========================================");
+
+        int age = 25;
+        boolean eligibility = (age >= 21 ) ?  true : false;
+        /*
+        if(age>= 21){
+            eligibility = true;
+        }else{
+            eligibility = false;
+        }
+        */
+
+        System.out.println(eligibility);
+
+        System.out.println("=====================================================");
+        int ageOfPerson = 20;
+        String eligibleToVote = "";
+
+        if(ageOfPerson >= 18){
+            eligibleToVote = "YES";
+        } else {
+            eligibleToVote = "NO";
+        }
+
+        String eligibleToVote2 = (ageOfPerson >= 18) ? "YES" : "NO";
+
+        System.out.println(eligibleToVote);
+        System.out.println(eligibleToVote2);
 
     }
 
