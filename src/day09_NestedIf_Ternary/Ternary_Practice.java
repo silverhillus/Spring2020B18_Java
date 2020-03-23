16c0185a0a8895c7834715b17ccc8e7cabc11247
cybertekschool <github@cybertekschool.com>
2020-03-23 13:00:52 -0400

    Day09 class

--- src/day09_NestedIf_Ternary/Ternary_Practice.java
+++ src/day09_NestedIf_Ternary/Ternary_Practice.java
@@ -48,7 +48,19 @@ public class Ternary_Practice {
         System.out.println(grade2);
 
 
+        System.out.println("================================================================");
 
+        int num =1000;
+
+        if(num %2 ==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+
+     //   (num %2 ==0) ? System.out.println("Even number") : System.out.println("Odd number");
+
+        // we can only apply ternary: if the if statement' body is only returning value and assigning it to variable then we can apply ternary
 
     }
 }
