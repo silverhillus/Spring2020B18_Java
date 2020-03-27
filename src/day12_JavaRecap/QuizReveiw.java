5956a6a8ba57a117ac9e0ef156e71ba5d0ef8fd0
cybertekschool <github@cybertekschool.com>
2020-03-27 14:51:53 -0400

    Day12 Class

---
+++ src/day12_JavaRecap/QuizReveiw.java
@@ -0,0 +1,27 @@
+package day12_JavaRecap;
+
+public class QuizReveiw {
+
+
+    public static void main(String[] args) {
+        boolean result = true;
+        int N = 100;
+
+        if(result){
+            N /= 10;   // N = 10
+            result = !result;  // result = false
+
+        }
+
+        if(result){
+            N *= 2;
+        }else{
+            N -= 5;  // N = 5
+        }
+
+
+
+
+    }
+
+}
