25320d58707746fc7b4f7329efe0453c3a0360c0
cybertekschool <github@cybertekschool.com>
2020-03-31 11:59:41 -0400

    Day14 Class

--- src/day14_StringClass/StringManipulations.java
+++ src/day14_StringClass/StringManipulations.java
@@ -13,7 +13,7 @@ public class StringManipulations {
         int a2     =  str.indexOf("Jum");
         System.out.println(a2);
 
-        int a3 = str.indexOf(" I") + 1  ; // second I' index number
+        int a3 = str.indexOf(" I") +1 ; // second I' index number
         System.out.println(a3);
 
         String s = "I like Java, and I like reading";
@@ -23,6 +23,42 @@ public class StringManipulations {
 
         int b2= s.lastIndexOf("l");
         System.out.println(b2);
+
+
+        String z = "I like C#, C# is cool";
+
+        int c1 = z.lastIndexOf("C");
+
+        System.out.println(c1);
+
+        String x = "I like Java, Java is fun, Java programming is fun";
+
+        int d1 = x.indexOf("Java is");  // second J' index num
+        System.out.println(d1);
+
+        int d2 = x.indexOf(", J")+2;   // second J' index num
+        System.out.println(d2);
+
+        int d3 = x.lastIndexOf("J"); // last J' index number
+        System.out.println(d3);
+
+        int d4 = x.indexOf("Java p"); // last J' index number
+        System.out.println(d4);
+
+
+             char ch1 =    x.charAt(26);
+
+        System.out.println(ch1);
+
+
+
+
+
+
+
+
+
+
 
 
 
