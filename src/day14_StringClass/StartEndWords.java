b0a17a38fe51bb92139c6a475b0dd5e5c71fea28
cybertekschool <github@cybertekschool.com>
2020-03-31 16:38:26 -0400

    Day14 Class

--- src/day14_StringClass/StartEndWords.java
+++ src/day14_StringClass/StartEndWords.java
@@ -14,7 +14,7 @@ public class StartEndWords {
         if last letter of 1st word and first letter of 2nd word match:  print "Fizz"
         if they do not match:  print "Buzz - did not match"
 
-        pre condition: both words MUST have 5 characters
+        pre condition: both words MUST have exact 5 characters
      */
 
     public static void main(String[] args) {
@@ -25,6 +25,20 @@ public class StartEndWords {
         System.out.println("Enter second word");
         String word2 = input.next();
 
+        int length1 = word1.length();
+        int length2 =word2.length();
+
+        if(length1 ==5 && length2 ==5){
+
+            if( word1.charAt( length1-1 ) ==  word2.charAt(0) ){
+                System.out.println("Fizz");
+            }else{
+                System.out.println("Buzz");
+            }
+
+        }else{
+            System.out.println("need to be exactly 5 chars length");
+        }
 
 
     }
