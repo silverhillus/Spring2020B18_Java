7709f53af736b7d94ed832bd04d1604b6b364d4d
cybertekschool <github@cybertekschool.com>
2020-04-01 16:07:22 -0400

    Office Hour: 04/01/2020

--- src/OfficeHours/Practice_04_01_2020/StringMethods.java
+++ src/OfficeHours/Practice_04_01_2020/StringMethods.java
@@ -47,9 +47,74 @@ public class StringMethods {
 
         System.out.println("=============================================");
 
-        //toLowerCase() & toUpperCase(): change the case of the strings to upper or lower  cases
+        //toLowerCase() & toUpperCase(): change the case of the strings to upper or lower  cases. returns new string
 
-       String str5 = "";
+       String str5 = "CYBERTEK SCHOOL";
+                str5 =    str5.toLowerCase(); // "cybertek school"
+
+        System.out.println(str5);
+
+        String str6 = "muhtar";
+           str6 =  str6.toUpperCase();//  "MUHTAR";
+
+        System.out.println(str6);
+
+        System.out.println("=============================================");
+
+        // trim(): removes unused(white) spaces from the string, returns new String
+        String str7 = "        Cybertek          ";
+                    str7  =    str7.trim();// "Cybertek";
+
+        System.out.println(str7);
+        System.out.println(str7.length());
+
+        String str8 = "       ";
+                str8 = str8.trim(); // ""
+
+        System.out.println( str8.length() ); //0
+
+        System.out.println("=============================================");
+
+        //substring(beginningindex, ending index):  creates substring strating from beginning index till ending index (ending index is excluded)
+        String sentence1 = "I like Java";
+                //          012345678910
+        //last index number = length-1
+
+        String word1 =  sentence1.substring(7, sentence1.length() ); // "Java"
+        String word2 = sentence1.substring(2, 6);
+
+                        // "likeJava"
+        String word3 = sentence1.substring(2, 6)+ sentence1.substring(7, sentence1.length() );
+
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
+
+        //substring(begiining index): creates the sub string from beginning index till the end of the string. returns new string
+        String sentence2 = "I like to learn Java";
+        //                  0123456789
+
+        String r1 =  sentence2.substring(10);  // "learn Java";
+        System.out.println(r1);
+
+        String r2 = "Java"; //JaVa;
+            //       0123
+        String r3 = r2.substring(0, 2);   //Ja
+
+        String r4 = r2.substring(2,3) ;  // v
+                r4 = r4.toUpperCase(); //V
+
+        String r5 = r2.substring(3); //a
+
+        String result = r3+r4+r5; // JaVa
+
+        System.out.println(result);
+
+
+        System.out.println("=============================================");
+
+
+
 
 
 
