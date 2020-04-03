0aa87b0553bfd52cb49d556a627d2baa6d1b8161
cybertekschool <github@cybertekschool.com>
2020-04-03 10:42:43 -0400

    Day16 class

---
+++ src/day16_ForLoop/DivisibleBy3_5.java
@@ -0,0 +1,23 @@
+package day16_ForLoop;
+
+public class DivisibleBy3_5 {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 100; i++ ){
+
+            if(i % 2 != 0) {
+                if(i %3 ==0 && i %5 ==0) {
+                    System.out.print(i + " ");
+                }
+            }
+
+        }
+
+
+
+
+
+    }
+
+}
