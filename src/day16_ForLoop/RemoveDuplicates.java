505fc90cc4e43aea61a865c1d32c7b57aef7da24
cybertekschool <github@cybertekschool.com>
2020-04-03 15:37:47 -0400

    Day16 class

--- src/day16_ForLoop/RemoveDuplicates.java
+++ src/day16_ForLoop/RemoveDuplicates.java
@@ -4,10 +4,10 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
-        String str = "AAAABBBBBCCCCDDDDDAAABBBBBCCC";
+        String str = "ABAB";
 
 
-        String result = "";  // "CD"
+        String result = "";  // "AB" we store the expected result in this variable
 
         for(int i=0; i <= str.length()-1 ; i++){  // 0 , 1, 2 ,3
             /*
@@ -18,6 +18,7 @@ public class RemoveDuplicates {
              */
 
             if(result.contains( ""+str.charAt(i) )){
+                // if the string result does not contains str.charAt(i), then we concate it to the result, if it does we will not concate it to the result
                 continue;
             }
 
