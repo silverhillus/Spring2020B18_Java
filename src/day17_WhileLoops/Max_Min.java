bbd5883358b9e2fb19815c589334e4444c6e5646
cybertekschool <github@cybertekschool.com>
2020-04-06 10:50:22 -0400

    Day17 class

--- src/day17_WhileLoops/Max_Min.java
+++ src/day17_WhileLoops/Max_Min.java
@@ -14,11 +14,15 @@ public class Max_Min {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int MaxNumber = -1999999999;  // any number the user provided will be greater than this number
+        int MaxNumber = -2147483647;  // any number the user provided will be greater than this number
                         // first user entered variable will be initalized to MaxNumber
                 //MaxNumber = 5;
 
+        int MinNumber = 2147483647;
+            // MinNumber = 0;
+
        for(int i =1; i <= 10; i++) { // i: 1, 2, 3,4,5
+
            System.out.println("Enter a number");
            int num = input.nextInt();  // 0, 5, 1, 3, 0
 
@@ -26,10 +30,15 @@ public class Max_Min {
                MaxNumber = num;
            }
 
+           if(num < MinNumber){ // to compare each user inputs , and assign the minimum number to MinNumber
+              MinNumber = num;
+           }
+
        }
 
 
-        System.out.println(MaxNumber);
+        System.out.println("Maximum Number: "+MaxNumber);
+        System.out.println("Minimum Number: "+MinNumber);
 
 
 
