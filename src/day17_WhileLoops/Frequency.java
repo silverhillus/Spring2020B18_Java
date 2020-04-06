dd9161a449a33cf58d844af5a9d955a4e58d9b53
cybertekschool <github@cybertekschool.com>
2020-04-06 16:53:41 -0400

    Day17 class

--- src/day17_WhileLoops/Frequency.java
+++ src/day17_WhileLoops/Frequency.java
@@ -4,17 +4,19 @@ public class Frequency {
 
     public static void main(String[] args) {
 
-        String str = "EEee".toLowerCase();  //  is fun is cool
+        String str = "Cybertek School is a great place to study".toLowerCase();  //  is fun is cool
 
         int count = 0 ;
 
-        String word = "E".toLowerCase();
+        String word = "s".toLowerCase();  // ignoring the case sensitiveity by converting both string to lowercase
 
         while(str.contains(word)){
+
             count++;
             str = str.replaceFirst(word, ""); // after counting the first "Java",w e need to rmeove it from the str
 
         }
+
 
         System.out.println(count);
 
