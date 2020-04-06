a6542aca11ea79b16442eebdfce6695917408ae3
cybertekschool <github@cybertekschool.com>
2020-04-06 11:30:32 -0400

    Day17 class

--- src/day17_WhileLoops/FINRA.java
+++ src/day17_WhileLoops/FINRA.java
@@ -18,15 +18,19 @@ public class FINRA {
         for(int i = 1; i <= 30; i++){
 
                  if( i % 3 ==0 && i % 5 ==0){  // prints the numbers that are divisible by both 3 & 5
+
                     // System.out.print("FINRA"+" ");
                      result += "FINRA "; // concatenation
                  }else if( i % 3 == 0 ){  // prints the numebrs that are ONLY divisible by 3
+
                    //  System.out.print("FIN"+" ");
                      result += "FIN "; // concatenation
                  }else if(i % 5 == 0 ){  // prints the numebrs that are ONLY divisible by 5
+
                     // System.out.print("RA"+" ");
                      result += "RA "; // concatenation
                  }else{  // prints the numebrs that are not divisivle by either 3 or 5
+
                    //  System.out.print(i+" ");
                      result += i+" ";   // concatenation
                  }
