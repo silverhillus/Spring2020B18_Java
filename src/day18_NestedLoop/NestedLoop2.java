72845347fe9324fe8d637775b769e3bf884a5cd5
cybertekschool <github@cybertekschool.com>
2020-04-07 14:56:41 -0400

    Day17 class

---
+++ src/day18_NestedLoop/NestedLoop2.java
@@ -0,0 +1,52 @@
+package day18_NestedLoop;
+
+public class NestedLoop2 {
+
+    public static void main(String[] args) {
+
+        /*
+            *
+            * *
+            * * *
+            * * * *
+            * * * * *
+            * * * * * *
+            * * * * * * *
+            * * * * * * * *
+            * * * * * * * * *
+
+         */
+
+        for(int j= 1; j <= 9; j++){
+
+            for(int i =1; i <= j; i++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+        /*
+
+
+         * * * * * * * * *
+         * * * * * * * *
+         * * * * * * *
+         * * * * * *
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+
+         */
+
+
+
+
+
+
+    }
+
+}
