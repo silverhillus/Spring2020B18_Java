fc8cda54fcebfcf89b23dd5b101c1ba3c954247b
cybertekschool <github@cybertekschool.com>
2020-04-07 15:25:02 -0400

    Day17 class

--- src/day18_NestedLoop/NestedLoop2.java
+++ src/day18_NestedLoop/NestedLoop2.java
@@ -29,7 +29,6 @@ public class NestedLoop2 {
 
         /*
 
-
          * * * * * * * * *
          * * * * * * * *
          * * * * * * *
@@ -41,6 +40,53 @@ public class NestedLoop2 {
          *
 
          */
+        System.out.println("============================");
+
+        for(int j= 9; j >= 0; j-- ){
+
+            for(int i =1; i <= j; i++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+        System.out.println("============================");
+
+        /*
+            a
+            a b
+            a b c
+            a b c d
+            a b c d e
+
+            ......
+            a  .......     z
+
+         */
+
+        for(char z = 'a'; z <= 'z'; z++){
+
+            for(char ch ='a'; ch <= z ; ch++ ){  //  'a' >= 'a'
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+
+
+        }
+
+        System.out.println("=======================================");
+
+        for(char z = 'z'; z >= 'a'; z--){
+
+            for(char ch ='a'; ch <= z ; ch++ ){  //  'a' >= 'a'
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+
+
+        }
+
 
 
 
