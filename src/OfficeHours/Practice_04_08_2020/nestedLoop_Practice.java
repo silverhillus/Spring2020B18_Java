c524b3d53b558909e644b105db2ba2d4785ac62d
cybertekschool <github@cybertekschool.com>
2020-04-08 16:43:55 -0400

    Office hour: 04/08/2020

---
+++ src/OfficeHours/Practice_04_08_2020/nestedLoop_Practice.java
@@ -0,0 +1,61 @@
+package OfficeHours.Practice_04_08_2020;
+
+public class nestedLoop_Practice {
+
+    public static void main(String[] args) {
+        /*
+            nested loop: loop inside another loop
+                        inner loop & outer loop
+
+                nested loop: we only need them when we need to repeat a loop
+         */
+
+
+        int j = 1;
+        while (j < 6) {
+
+            for (int i = 10; i <= 50; i += 10) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+
+            j++;
+        }
+
+
+        System.out.println("=================================================");
+        /*
+         * * * * * *
+         * * * * * *
+         * * * * * *
+         * * * * * *
+         * * * * * *
+         * * * * * *
+         * * * * * *
+         * * * * * *
+
+         */
+
+        for(int l = 1;  l <= 15; l++){
+
+            for (int k = 0; k <= 20; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
