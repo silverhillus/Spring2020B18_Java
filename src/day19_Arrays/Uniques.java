bf53d7a613273ebcb2c370c4f3d86231780753ea
cybertekschool <github@cybertekschool.com>
2020-04-09 10:42:52 -0400

    Day19 Class

--- src/day19_Arrays/Uniques.java
+++ src/day19_Arrays/Uniques.java
@@ -6,28 +6,43 @@ package day19_Arrays;
 
  */
 
+import java.util.Scanner;
+
 public class Uniques {
 
     public static void main(String[] args) {
-        String str = "AZBAZBCDERTYQA";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String str = input.next();
 
-        String result = ""; //"B"
+        String result = ""; //"C", to store the unique characters
 
 
-        for(int  j = 0; j <= str.length()-1; j++ ){
-            int count = 0;
-            for(int i=0; i <= str.length()-1; i++){
-                if(str.charAt(i) == str.charAt(j)){
+        for(int j=0; j <= str.length()-1; j++ ){
+            char ch2 = str.charAt(j);
+            int count = 0 ;  // to count the occurence of the character
+
+            for(int i = 0; i <= str.length()-1; i++){
+                char ch = str.charAt(i);
+                if(ch == ch2){  // check how mnay time the character is  occured in the string
                     count++;
                 }
             }
-            if(count == 1){  // if unique
-                result+= str.charAt(j);
+
+            if(count == 1){  // the the character at index J is unique , will be concated to thre sult
+                result+=ch2;
             }
 
         }
 
+
         System.out.println(result);
+
+
+
+
+
+
 
 
 
