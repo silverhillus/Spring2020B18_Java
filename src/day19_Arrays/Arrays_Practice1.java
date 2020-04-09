18ec3e053aa4601f936fd8a5128da19b3572f5fa
cybertekschool <github@cybertekschool.com>
2020-04-09 12:31:15 -0400

    Day19 Class

--- src/day19_Arrays/Arrays_Practice1.java
+++ src/day19_Arrays/Arrays_Practice1.java
@@ -4,8 +4,8 @@ public class Arrays_Practice1 {
 
     public static void main(String[] args) {
 
-        String[] friends = { "Kuzzat" , "Asiya" , "Saim" , "Akbar", "Marufjon", "Guljannat" , "Murodil"};
-                    //          0          1         2        3        4             5           6
+        String[] friends = {"Kuzzat", "Asiya", "Saim", "Akbar", "Marufjon", "Guljannat", "Murodil"};
+        //          0          1         2        3        4             5           6
 
         /*
         System.out.println(   friends[6]   );
@@ -15,14 +15,25 @@ public class Arrays_Practice1 {
 
          */
 
-        for(int i = 0; i <= 6; i++){
-           String name = friends[i];
+        for (int i = 0; i <= 6; i++) {
+            String name = friends[i];
             System.out.println(name);
         }
 
 
+        String[] emails = {"cybertek@yahoo.com", "cybertek@gmail.com", "cybertek@hotmail.com", "cybertek@outlook.com"};
 
+        // print out all the users who regsitered with thier gmail
 
+        //  max:  4
+
+        for (int i = 0; i <= 3; i++) {
+            String email = emails[i];
+            if (email.endsWith("@gmail.com")) {
+                System.out.println(email);
+            }
+
+        }
 
 
 
