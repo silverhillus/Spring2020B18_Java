c888d2b7a15b0306146805293eef39fc501cd76d
cybertekschool <github@cybertekschool.com>
2020-04-09 11:27:07 -0400

    Day19 Class

--- src/day19_Arrays/Frequency.java
+++ src/day19_Arrays/Frequency.java
@@ -19,12 +19,29 @@ public class Frequency {
 
         }
 
-        System.out.println(nonDup);
+      //  System.out.println(nonDup);
+
+
+    // nonDup ="ABC";     str = "AABCC";   ==> result= "A2B1C2"
+
+        for(int j = 0 ; j <= nonDup.length()-1; j++ ){
+            char ch = nonDup.charAt(j);
+
+            int count = 0;  // count the total number of occurence of each charcter from str
+            for(int i = 0; i <= str.length()-1; i++){
+                if(str.charAt(i) == ch){
+                    count++;
+                }
+            }
+            result +=   ""+ ch  + count;
+
+        }
 
 
 
+        System.out.println(result);
 
-        int count = 0;  // count the total number of occurence of each charcter from str
+
 
 
 
