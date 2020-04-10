2a67a4d9aa3a6dfc7aec73eb5c0df1d2bf1b1f61
cybertekschool <github@cybertekschool.com>
2020-04-10 11:27:27 -0400

    Day20 Class

--- src/day20_ArraysContinue/Days.java
+++ src/day20_ArraysContinue/Days.java
@@ -9,11 +9,20 @@ public class Days {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
-            int num  =  input.nextInt();  // 6
+            int num  =  input.nextInt();  // 18
 
-        if(num >7 || num < 1){
-            System.out.println("Invalid Entry");
-            System.exit(0);
+        int attepmts = 1;
+        while(num >7 || num < 1){
+
+            System.out.println("Invalid Entry, please re enter the number");
+            num = input.nextInt();  // 6
+            attepmts++;
+
+            if(attepmts == 3 && (num >7 || num < 1) ){
+                System.exit(0);
+            }
+
+
         }
 
 
