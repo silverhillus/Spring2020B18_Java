4e3eb7102f23395a08f5ac6b9ac41a8c49909bc9
cybertekschool <github@cybertekschool.com>
2020-04-10 10:21:04 -0400

    Day20 Class

--- src/day20_ArraysContinue/Max_Min.java
+++ src/day20_ArraysContinue/Max_Min.java
@@ -18,6 +18,7 @@ public class Max_Min {
 
         for(int i = 0; i <= lastindex; i++  ){
 
+
             if(arr[i] >= max){  // array's index are compared with each other, and whichever is greater will be assigned to the max
                 max = arr[i];
             }
