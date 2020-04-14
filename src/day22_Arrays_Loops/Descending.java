4f1307c6a2605a4eefa75de75e5b587063e31106
cybertekschool <github@cybertekschool.com>
2020-04-14 11:42:08 -0400

    Day22 Class

--- src/day22_Arrays_Loops/Descending.java
+++ src/day22_Arrays_Loops/Descending.java
@@ -1,7 +1,25 @@
 package day22_Arrays_Loops;
 
+import java.util.Arrays;
+
 public class Descending {
 
+    public static void main(String[] args) {
+        int[] arr = {10, 78, 2, -1, 500, 70};
+        Arrays.sort(arr); // sorts in ascending
 
+        // System.out.println( Arrays.toString(arr) );
+
+        int[] RevArr = new int[arr.length];
+
+        int j = arr.length -1;
+        for(int i =0; i <= arr.length-1; i++ ){
+            RevArr[i] = arr[j];
+            j--;
+        }
+
+        System.out.println( Arrays.toString(RevArr));
+
+    }
 
 }
