b16baca6b451059d7acfe3c47ea074d7451139b6
cybertekschool <github@cybertekschool.com>
2020-04-14 15:42:35 -0400

    Day22 Class

--- src/day22_Arrays_Loops/NestedLoops2.java
+++ src/day22_Arrays_Loops/NestedLoops2.java
@@ -22,12 +22,49 @@ public class NestedLoops2 {
         }
 
 
+
+        //numbers = {  {9, 8, 7},   {6},  {5,4,3,2,1,0}   };
+        //                 0         1         2
+
         // 7 8 9 6 0 1 2 3 4 5
+
+        for(int k=0; k < numbers.length; k++ ){ //k: index num of 1D arrays
+
+            for(int i = numbers[k].length-1;  i >=0; i-- ){ // i: index num of elements in 1D array
+                System.out.print(numbers[k][i] +" ");
+            }
+
+        }
+
+        System.out.println();
+
+
+        //numbers = {  {9, 8, 7}, {6},  {5,4,3,2,1,0}   };
+        //                 0         1         2
 
         // 5 4 3 2 1 0 6 9 8 7
 
+        for(int k = numbers.length-1; k >= 0; k--){ //k: index num of 1D arrays (reversed)
+
+            for(int i = 0;  i < numbers[k].length; i++){ // i: index num of elements in 1D array
+                System.out.print(numbers[k][i] +" ");
+            }
+
+        }
+
+        System.out.println();
+
+        //numbers = {  {9, 8, 7}, {6},  {5,4,3,2,1,0}   };
+        //                 0         1         2
         // 0 1 2 3 4 5 6 7 8 9
 
+        for(int k = numbers.length-1; k >= 0; k--) { //k: index num of 1D arrays (reversed)
+
+            for(int i = numbers[k].length-1;  i >=0; i-- ){ // i: index num of elements in 1D array (reversed)
+                System.out.print(numbers[k][i]+" ");
+            }
+
+        }
 
 
 
