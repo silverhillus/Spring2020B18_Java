a97e4553f85f27838a6d59c71e068e7f98fe83af
cybertek <github@cybertekschool.com>
2020-04-20 14:36:53 -0400

    Day25 Class

--- src/Resources/Library.java
+++ src/Resources/Library.java
@@ -39,6 +39,21 @@ public class Library {
 
     // min number from array
 
+    public static String RemoveDuplicates(String str){
+        String result =  "";      //AB
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i); //B
+            if( !result.contains(""+ch) ){
+                result += ch;
+            }
+
+        }
+
+        return result;
+    }
+
+
 
 
 
---
+++ src/day25_MethodsRecap/Test.java
@@ -0,0 +1,17 @@
+package day25_MethodsRecap;
+
+import Resources.Library;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String str = "ABCDABABABABA";
+        String result = Library.RemoveDuplicates(str);
+
+        System.out.println(result);
+
+
+    }
+
+}
