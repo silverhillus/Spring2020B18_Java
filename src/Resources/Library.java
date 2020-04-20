d1eb7df164d8bd6f0808774779025987a9cc334d
cybertek <github@cybertekschool.com>
2020-04-20 14:58:20 -0400

    Day25 Class

--- src/Resources/Library.java
+++ src/Resources/Library.java
@@ -53,6 +53,16 @@ public class Library {
     }
 
 
+    public static int Frequency(String str1, String str2){
+        int count = 0 ;
+        while(str1.contains(str2)){
+            count++;
+            str1 = str1.replaceFirst(str2, "");
+            // we need to make that we are not counting the same index over again
+        }
+        return count;
+    }
+
 
 
 
