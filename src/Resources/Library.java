be1defe54e791c7c5529f8d1032b678808230262
cybertek <github@cybertekschool.com>
2020-04-20 15:41:20 -0400

    Day25 Class

--- src/Resources/Library.java
+++ src/Resources/Library.java
@@ -64,6 +64,20 @@ public class Library {
     }
 
 
+    public static String FrequencyOfChars(String str){
+        String NonDup =  Library.RemoveDuplicates(str)  ;             //ABC
+        String result = ""; //coantisn the frequency of chars
+
+        for(int i =0; i < NonDup.length();i++){
+            String ch = ""+NonDup.charAt(i);//  "A"
+            int num =  Library.Frequency(str, ch);
+            result += ch+num;
+        }
+
+        return result;
+    }
+
+
 
 
 }
