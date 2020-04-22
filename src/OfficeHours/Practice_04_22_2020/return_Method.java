f91107d20f515184960060c31d667576f6d485b6
cybertekschool <github@cybertekschool.com>
2020-04-22 15:47:58 -0400

    Office hour: 04/22/2020

--- src/OfficeHours/Practice_04_22_2020/return_Method.java
+++ src/OfficeHours/Practice_04_22_2020/return_Method.java
@@ -3,13 +3,30 @@ package OfficeHours.Practice_04_22_2020;
 public class return_Method {
 
     public static void main(String[] args) {
-       //int a = sum(10,20);
+       //int a = sum(10,20);  // void
 
-       int num1 = sum2(500, 600);
+       int num1 = sum2(500, 600);  // return method
 
         System.out.println(num1);
 
 
+        System.out.println("=====================================");
+        String str = "Muhtar" ;
+
+        String reuslt = "";
+        for(int i = str.length()-1; i>=0; i--){
+            reuslt += str.charAt(i);
+        }
+
+
+        System.out.println(reuslt);
+
+        String name = "level";
+        String name2 = reverse(name);
+
+        System.out.println(name2);
+
+        System.out.println( name.equals(name2) );
 
 
     }
@@ -18,11 +35,18 @@ public class return_Method {
         System.out.println(a+b);
     }
 
-
-
     public static int sum2(int a, int b){
 
         return a+b;
+    }
+
+
+    public static String reverse(String str){
+        String reuslt = "";
+        for(int i = str.length()-1; i>=0; i--){
+            reuslt += str.charAt(i);
+        }
+        return reuslt;
     }
 
 
