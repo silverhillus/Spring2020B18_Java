cdd708546a69b6c3553848ed22b6d1ec261f6eff
cybertekschool <github@cybertekschool.com>
2020-04-23 12:51:02 -0400

    Day27 Class

--- src/day27_DateTime/Dates_Formatting.java
+++ src/day27_DateTime/Dates_Formatting.java
@@ -17,9 +17,20 @@ public class Dates_Formatting {
         /*
         create a date called birthDay: month/Days/year
                 eX: 2020 04  23
-                    Aprl/23/20
+                    Apr/23/20
          */
 
+
+        DateTimeFormatter dtf2 =DateTimeFormatter.ofPattern("E MMMM/dd/yy");
+
+        LocalDate date2 = LocalDate.of(1999, 11, 25);
+        String str1 = date2.format(dtf2);
+
+        System.out.println(str1);
+
+        LocalDate date3 =LocalDate.now();
+
+        System.out.println( date3.format(dtf2) );
 
 
 
