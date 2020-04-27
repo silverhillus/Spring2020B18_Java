43aecd76ca1a0d9e6561c5ced74f9e03b6734f31
cybertekschool <github@cybertekschool.com>
2020-04-27 12:53:54 -0400

    Day29 Class

--- src/day29_Wrapper_ArrayList/ValueOfMethods.java
+++ src/day29_Wrapper_ArrayList/ValueOfMethods.java
@@ -1,5 +1,7 @@
 package day29_Wrapper_ArrayList;
 
+import java.util.Arrays;
+
 public class ValueOfMethods {
 
     public static void main(String[] args) {
@@ -42,10 +44,44 @@ public class ValueOfMethods {
       "True"
       "FAlSe" ==> to boolean
       "1000000000.5" ==> to float
-      "123456"  ==> to short
+      "123456"  ==> to int
        */
 
+        String r1 = "TrUE";
+        Boolean t1 = Boolean.valueOf(r1); // Boolean, true
+                // Boolean = Boolean  // none
 
+        boolean t2 = Boolean.valueOf(r1); // Boolean, true
+        // boolean = Boolean  // unboxing
+
+        System.out.println(t1);
+        System.out.println(t2);
+
+        System.out.println("============================================");
+        int maxNum = Integer.MAX_VALUE;
+
+        System.out.println(maxNum);
+
+        double maxNum2 = Double.MAX_VALUE;
+
+        System.out.println(maxNum2);
+
+        int minNum = Integer.MIN_VALUE;
+        System.out.println(minNum);
+
+        byte maxNum3 = Byte.MAX_VALUE; // 127
+        System.out.println(maxNum3);
+
+        byte minNum2 = Byte.MIN_VALUE;   // -128
+        System.out.println(minNum2);
+
+        System.out.println("=================================");
+
+        char[] arr = new char[3];
+        System.out.println(Arrays.toString(arr) );
+
+        Integer[] arr2 = new  Integer[3];
+        System.out.println(Arrays.toString(arr2) );
 
 
     }
