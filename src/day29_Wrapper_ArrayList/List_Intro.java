b151339eb36263c38f71aeddc1e0f74bbe65e4da
cybertekschool <github@cybertekschool.com>
2020-04-27 15:46:15 -0400

    Day29 Class

--- src/day29_Wrapper_ArrayList/List_Intro.java
+++ src/day29_Wrapper_ArrayList/List_Intro.java
@@ -32,6 +32,8 @@ public class List_Intro {
         1. create a list of Integers
         2. add 5 Integers to it
         3. return the maximum number from the list
+
+                Do not use any sorting
          */
 
 
